8666326e5d9c7786c5fe72b31b55ebdf41e2452e
FS <fstriewski@outlook.com>
2019-04-25 18:13:36 +0200

    Attribute inheritance, subclass

--- char.py
+++ char.py
@@ -1,11 +1,35 @@
 # This is a basic version of the char class
+import dice
 
 
 class Character():
-    def __init__(self, name, life, strength, inventory):
+    def __init__(self,
+                 name: str = '',
+                 life: int = 60,
+                 strength: int = 10 + dice.dice_d(3, 3),
+                 inventory: list = [],
+                 ) -> None:
+        self.type = 'Adventurer'
         self.name = name
         self.life = life
         self.strength = strength
         self.inventory = inventory
-    def getvalues(self):
-        print(self.name, self.strength)
+
+    def stats(self):
+        print('Name: "{}" Class: "{}" \nLife: [{}] Strength: [{}]'.format(
+              self.name,
+              self.type,
+              self.life,
+              self.strength))
+
+    def print_inventory(self):
+        print()
+
+
+class Mechanic(Character):
+    def __init__(self):
+        super().__init__()
+        self.type = 'Mechanic'
+        self.inventory = ['Hammer']
+
+
--- init.py
+++ init.py
@@ -1,22 +1,11 @@
 # This is the root file that starts the game
-import dice
 import char
 
-
-character = char.Character('', 0, 0, []  )
+# create an instance of the Character class:
+character = char.Mechanic()
 
 
 print('Create a new character: ')
 
 character.name = input('Enter a name: ')
-character.life = 60
-character.strength = 10 + dice.dice_d(3,3)
-# character.getvalues()
-print('Your chars name is {} with {} life and {} strength'.format(character.name,
-                                                                   character.life,
-                                                                   character.strength))
-
-
-
-
-#dice.dice_d6()
+character.stats()
