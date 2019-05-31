e510a17881948d0d205f53f41c8be1b90f24dfb1
FS <fstriewski@outlook.com>
2019-05-31 18:05:44 +0200

    Extend Room/Choice logic

--- rooms/room.py
+++ rooms/room.py
@@ -1,11 +1,59 @@
 # Room definition
+import reader
+
+
+class Choices():
+    PATH = '../txt/rooms.txt'
+
+    def __init__(self, room_id):
+
+        self.room_id = room_id
+        self.choices = [
+            {
+             'id': 'A',
+             'descriptor': self.reader_lookup('A'),
+             'exhausted': 'false',
+             },
+            {
+             'id': 'B',
+             'descriptor': self.reader_lookup('B'),
+             'exhausted': 'false',
+            },
+            {
+             'id': 'C',
+             'descriptor': self.reader_lookup('C'),
+             'exhausted': 'false',
+
+            }
+        ]
+
+    def reader_lookup(self, id):
+        return reader.parse(self.PATH, self.room_id.concat(id))
+
+    def exhaust(self, id):
+        self.id['exhausted'] = 'true'
 
 
 class Room():
-    def __init__(self, name):
-        self.name = name
-        self.description = ''
-        self.id = ''
+    PATH = '../txt/rooms.txt'
+
+    def __init__(self, room_id, connectors, choices):
+        self.name = reader.parse(self.PATH, room_id.concat('N'))
+        self.description = reader.parse(self.PATH, room_id.concat('D'))
+        self.visited = 'false'
+        self.room_id = room_id
+        self.connectors = connectors
+        self.choices = Choices(room_id)
+
+    def set_visited(self):
+        self.visited = 'true'
+
+    def get_choices(self, choices):
+        # if 2/3 choices exhausted, reroute
+
+        for choice in choices:
+            reader.read(self.PATH, 'KEY')
+        # user pick choice - choices.exhaust(id)
 
 
 class Lobby(Room):
