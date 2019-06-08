2e74055e70d25cda71a24de5ab69d35b246a3754
FS <fstriewski@outlook.com>
2019-06-08 12:31:00 +0200

    Fix monster

--- monster.py
+++ monster.py
@@ -1,34 +1,45 @@
-import dice
 from lib import reader
 
 
 class Monster():
-    def __init__(self, life: int, strength: int, inventory, description: str = 'A monster appears'):
+    PATH = './txt/monster.txt'
+
+    def __init__(self, life: int, strength: int, monster_id: str, inventory):
         self.life = life
         self.initial_life = self.life
         self.strength = strength
+        self.monster_id = monster_id
         self.inventory = inventory
-        self.description = description
-        self.lost_fight = 'It takes a last breath and dies.'
-        self.won_fight = 'The last what you see of it are its evil eyes.'
-
-    def attack(self):
-        print('The monster attacks')
 
     def print_description(self):
-        print(self.description)
+        '''Description on encounter'''
+        reader.parse(self.PATH, ''.join((self.monster_id, 'D')))
+
+    def attack(self):
+        '''Monster wins an attack'''
+        reader.parse(self.PATH, ''.join((self.monster_id, 'H')))
 
     def loot(self):
+        '''Player gets item after winning a fight'''
         print('You find nothing of interest')
 
     def take_hit(self, value):
+        '''Palyer hits monster'''
         self.life -= value
         if (self.life > 0):
+            reader.parse(self.PATH, ''.join((self.monster_id, 'T')))
             print('The monster takes a hit and has {}/{} health left.'
                   .format(self.life, self.initial_life))
         else:
-            print(self.lost_fight)
+            reader.parse(self.PATH, ''.join((self.monster_id, 'L')))
             self.loot()
 
 
-Ghoule = Monster(5, 5, [], reader.parse('./txt/monster.txt', '{MM00}'))
+# Beastary
+Dog = Monster(6, 0, 'MD0', [])
+Crows = Monster(5, 0, 'MC0', [])
+Rat = Monster(7, 0, 'MR0', [])
+Servents = Monster(7, 0, 'MS0', ['Golden Key'])
+Cook = Monster(6, 0, 'MO0', [])
+OldMan = Monster(8, 0, 'MM0', ['Silver Key'])
+Final = Monster(10, 0, 'MF0', [])
