02e0ab9056d747ef215bbdb6cfe872416f242981
FS <fstriewski@outlook.com>
2019-07-17 20:12:51 +0200

    Fix loader model

--- init.py
+++ init.py
@@ -1,14 +1,16 @@
 # This is the root file that starts the game
-import char
-from lib import reader
-from monster import Monster, Ghoule
-# create an instance of the Character class:
+# import char
+# from monster import Monster, Ghoule
+import room
 
-reader.read('./txt/story_line.txt', '{SL01}')
+room.stage_loader('L00')
 
-Ghoule.attack()
 
-character = char.char_selector()
-character.stats()
-character.print_description()
-character.print_inventory()
+# reader.read('./txt/story_line.txt', '{SL01}')
+
+# Ghoule.attack()
+
+# character = char.char_selector()
+# character.stats()
+# character.print_description()
+# character.print_inventory()
--- room.py
+++ room.py
@@ -2,6 +2,16 @@
 from lib import reader
 
 
+class Connectors():
+    PATH = 'TBD'
+
+    def __init__(self, room_id):
+        self.room_id = room_id
+    # if is router no choices
+    # if is blocking / max rounds no routing
+    # is connector - display choices
+
+
 class Choices():
     PATH = './txt/rooms.txt'
 
@@ -14,21 +24,17 @@ class Choices():
              'descriptor': self.reader_lookup('A'),
              'description': self.reader_lookup('X'),
              'exhausted': 'false',
-             },
-            {
+            },{
              'id': 'B',
              'descriptor': self.reader_lookup('B'),
              'description': self.reader_lookup('Y'),
              'exhausted': 'false',
-            },
-            {
+            },{
              'id': 'C',
              'descriptor': self.reader_lookup('C'),
              'description': self.reader_lookup('Z'),
              'exhausted': 'false',
-
-            }
-        ]
+            }]
 
     def reader_lookup(self, id):
         return reader.parse(self.PATH, ''.join((self.room_id, id)))
@@ -42,7 +48,7 @@ class Room():
         self.description = reader.parse(self.PATH, ''.join((room_id, 'D')))
         self.visited = 'false'
         self.room_id = room_id
-        self.connectors = None  # Connectors(room_id)
+        self.connectors = None # Connectors(room_id)
         self.choices = Choices(room_id).choices
 
     def init_room(self):
@@ -60,15 +66,19 @@ class Room():
 
     def get_choices(self):
         gen = (choice for choice in self.choices if choice['exhausted'] != 'true')
-        sel_list = []
+        print(gen)
         selection = ''
+        sel_list = []
 
         for choice in gen:
             sel_list.append(choice['id'])
-            print(choice['id'] + '    ' + choice['descriptor'])
+            print( choice['id'] + '    ' + choice['descriptor'])
         while selection.upper() not in sel_list:
-            selection = input('Select: ')
+            print(sel_list)
+            selection = input('Select: ').upper()
         self.exhaust_choice(selection)
+        # Needs to filter and call with id
+        print(selection)
         return None
 
 
@@ -77,5 +87,4 @@ def stage_loader(room_id):
     stage = Room(room_id)
     stage.init_room()
 
-    stage.get_choices()
     stage.get_choices()
